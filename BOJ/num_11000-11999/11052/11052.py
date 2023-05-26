5c995318fa9374926f398adcfd72ac4d04258ef4
ssoooooll <ssoooooll@naver.com>
2023-05-26 20:31:39 +0900

    230526_commit

---
+++ BOJ/num_11000-11999/11052/11052.py
@@ -0,0 +1,12 @@
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+num = [0] + list(map(int, input().split()))
+dp = [0 for _ in range(N+1)]
+
+for i in range(1, N+1) :
+    for k in range(1, i+1) :
+        dp[i] = max(dp[i], dp[i-k]+num[k])
+
+print(dp[N])
