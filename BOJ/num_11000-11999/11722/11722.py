185e67bef11534edb2aa915ee1e7e9214e3fbbba
ssoooooll <ssoooooll@naver.com>
2023-08-13 22:35:44 +0900

    230813_commit

---
+++ BOJ/num_11000-11999/11722/11722.py
@@ -0,0 +1,14 @@
+import sys
+input = sys.stdin.readline
+
+A = int(input())
+num = list(map(int, input().split()))
+
+dp = [1 for _ in range(A)]
+
+for i in range(1, A) :
+    for j in range(i) :
+        if num[i] < num[j] :
+            dp[i] = max(dp[i], dp[j] + 1)
+
+print(max(dp)) 
