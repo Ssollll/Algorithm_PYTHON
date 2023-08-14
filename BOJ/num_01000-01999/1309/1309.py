87aa7207c20f4164b26cfb26a284bdc03e29c5d3
ssoooooll <ssoooooll@naver.com>
2023-08-14 21:17:21 +0900

    230814_commit

---
+++ BOJ/num_01000-01999/1309/1309.py
@@ -0,0 +1,15 @@
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+
+dp = [1 for _ in range(N+1)]
+dp[1] = 3
+
+if N == 1 :
+    print(dp[1])
+else :
+    for i in range(2, N+1) :
+        dp[i] = (dp[i-2] + (2 * dp[i-1])) % 9901
+
+    print(dp[N])
